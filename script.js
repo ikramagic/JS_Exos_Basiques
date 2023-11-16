8df31602c931b387583690409dc1ab784ac52fb8
ikramiste <ikrame.saadi@gmail.com>
2023-11-16 12:24:54 +0100

    corrected typo in script 3 import

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import { valdHello } from "./script_1.js";
 import { youHello } from "./script_1_bis.js";
 import { factorial } from "./script_2.js";
-import { pyramid } from ".script_3.js";
+import { pyramid } from "./script_3.js";
 
 //récupère les fonctions depuis script_x.js
 
